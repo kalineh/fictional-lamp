d9e94b71422a656b13fd5d4298141d654606b6ba
kalin <kalin.eh@gmail.com>
2017-06-17 16:31:18 -0700

    initial commit

---
+++ Assets/CustomController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CustomController
+    : SteamVR_TrackedController
+{
+    public override void OnTriggerClicked(ClickedEventArgs e)
+    {
+        base.OnTriggerClicked(e);
+
+        var fwd = transform.forward;
+        var hitInfo = new RaycastHit();
+        var hit = Physics.Raycast(transform.position, transform.forward, out hitInfo);
+        if (hit)
+        {
+            transform.parent.DOMove(hitInfo.point, 0.12f).SetEase(Ease.Linear);
+        }
+    }
+
+    //public void Update()
+    //{
+        //Debug.DrawLine(transform.position, transform.position + transform.forward * 20.0f, Color.red);
+    //}
+}
---
+++ Assets/GhostController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostController
+    : MonoBehaviour
+{
+    public GameObject origin;
+
+    public void Update()
+    {
+        var indexLeft = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost);
+        var indexRight = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Rightmost);
+
+        var touchpad = Vector2.zero;
+
+        if (indexLeft != -1)
+            touchpad += SteamVR_Controller.Input(indexLeft).GetAxis();
+        if (indexRight != -1)
+            touchpad += SteamVR_Controller.Input(indexRight).GetAxis();
+
+        var planeX = origin.transform.right;
+        var planeZ = origin.transform.forward;
+
+        planeX.y = 0.0f;
+        planeZ.y = 0.0f;
+
+        planeX.Normalize();
+        planeZ.Normalize();
+
+        var moved = Vector3.zero;
+
+        moved += planeX * touchpad.x * Time.deltaTime * 1.5f;
+        moved += planeZ * touchpad.y * Time.deltaTime * 1.5f;
+
+        transform.position += moved;
+    }
+}
